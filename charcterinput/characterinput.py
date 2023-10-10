29070f6d0e1e5c8be980fbe7406dcbe15b406e11
Andrew Martins <andrew_omartins@yahoo.com>
2023-10-10 00:55:51 -0400

    characterinput

---
+++ charcterinput/characterinput.py
@@ -0,0 +1,16 @@
+# write a program that asks the user their name and their age. T
+# then have a message printed out to them that says when they will turn 100 years old 
+# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+# Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+
+
+# name = input("what the fuck is your name:  ")
+# print("nice to meet you: ", name)
+
+# age = int(input("how old are you again?"))
+# print("are you really: ", age, "years old?")
+
+# print(4 * "test")
+
+name = input("what is your name?")
